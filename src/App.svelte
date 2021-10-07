3910f851ae72ef18783e52e98ca766eaa5c92f32
Ian Findlay <code@findlayian.com>
2021-10-07 11:12:33 +0100

    Weight random memory choice towards earlier ones

--- src/App.svelte
+++ src/App.svelte
@@ -70,7 +70,7 @@
 	{/if}
 	{#if memoryIsOpen}
 		<Memory
-			memory={$memories[Math.floor(Math.random() * $memories.length)]}
+			memory={$memories[Math.floor(Math.pow(Math.random(), 1.3) * $memories.length)]}
 			close={closeMemory}
 			dele={deleteMemory}
 		></Memory>
